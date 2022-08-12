3e831b28d441c9ed0e3eae3a0f869ea0e245b2d4
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-08-12 23:33:45 +0530

    Update main.cpp

--- Floyd_Warshall_Algorithm/main.cpp
+++ Floyd_Warshall_Algorithm/main.cpp
@@ -1,27 +1,44 @@
 #include <stdio.h>
-#define I 999999
-
-int max(int a , int b ){ return (a>b)?  a : b ;}
-void print_result(int n , int graph[][4]){
-    for(int i = 0 ; i<n ; i++){
-        for(int j = 0 ; j <n ; j++){
-           printf("%d " , graph[i][j]);
-        }   
-    printf("\n");
+#define I 9999999
+int min(int a , int b) {
+    return (a>b)?b:a;
+}
+void printMat(int n , int grph[n][n]){
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            printf("%d " , grph[i][j]);
+        }
+        printf("\n");
     }
 }
-void floyd_war(int n , int graph[][4]){
-    for(int k = 0 ; k < n ; k++)
-        for(int i = 0 ; i < n ; i++)
-            for(int j = 0 ; k < n ; k++)
-                graph[i][j] = max(graph[i][j] , graph[i][k] + graph[k][j]);
-                print_result(n,graph);
+
+void warshall(int n , int grph[n][n]){
+    for(int k = 0 ; k < n ; k ++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                    grph[i][j] = min(grph[i][j] , grph[i][k]+grph[k][j]);
+            }
+        }
+    }
 }
+
 int main() {
-    int graph[][4] { {2,1,I,4},
-                     {1,0,9,I},
-                     {2,4,1,2},
-                     {I,I,I,800}
-    };
-    floyd_war(4 , graph);
+    int n = 0 ;
+    printf("\n Enter the size of the matrix: ");
+    scanf("%d" , &n);
+    int A[n][n];
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n ; j++){
+            scanf("%d" , &A[i][j]);
+        }
+    }
+    printf("\n");
+    printf("The matrix before transitive closure is \n: ");
+    printMat(n,A);
+    printf("\n");
+    warshall(n,A);
+    printf("The shortest pairs in the matrix is :\n ");
+    printMat(n,A);
+    printf("\n");
+    return 0;
 }
